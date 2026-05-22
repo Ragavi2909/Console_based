3f1efe3b4ef444b2a74d5f5f8333b9aee6140e9c
Ragavi2909 <rrgavi03@gmail.com>
2026-05-22 07:16:24 +0530

    provided grade according to the marks

---
+++ Student_Grade_System/grade.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class grade{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the name of the student: ");
+        String name = sc.nextLine();
+
+        System.out.print("Enter the marks gained by the student: ");
+        int marks = sc.nextInt();
+
+        if(marks> 100){
+            System.out.print("Invalid marks");
+        }
+        
+        if(marks>80 && marks<=100){
+            System.out.print("Grade A");
+        }
+
+        else if(marks>45 && marks<=80 ){
+            System.out.print("Grade B");
+        }
+
+        else if(marks>25 && marks<=45){
+            System.out.print("Grade C");
+        }
+
+        else{
+            System.out.print("Failed the exam");
+        }
+
+        sc.close();
+    }
+}
